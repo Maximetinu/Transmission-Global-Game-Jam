cbb49d70da2d0725cd5bda18e1316554053b3545
Lifka <javierizquierdovera@gmail.com>
2018-01-27 20:39:02 +0100

    Fix merge errors

--- Assets/_scripts/PlayerController.cs
+++ Assets/_scripts/PlayerController.cs
@@ -14,18 +14,16 @@ public class PlayerController : MonoBehaviour {
 	public float range = 4f;
 
 	//Parameter used for make configurable the radius increase of the light
-	private float LightRangeVariator = 2f;
+	private float LightRangeVariator = 0.5f;
 	private float LightCurrent;
-	private float LigthInitial = 18;
+	private float LigthInitial = 4;
 	private float LightMaxRange = 150f;
 	private float LightMinRange = 4f;
 
-<<<<<<< HEAD
 	// Fall
 	private float timeInAir = 0f;
  	public float FallingDeathTimer = 3f;
-=======
->>>>>>> 09a6263a8752df61877be5ca4825aeb3567e8023
+	private bool deathWhenGround = true;
 
 	private List<Vector2> flamesPositions = new List<Vector2>();
 
@@ -63,12 +61,24 @@ public class PlayerController : MonoBehaviour {
 	void FixedUpdate(){
 		Vector2 FixedVelocity = rigidbody.velocity;
 		FixedVelocity.x *= 0.75f;
+
 		if (grounded) {
 			rigidbody.velocity = FixedVelocity;
-			Debug.Log("timeInAir --> " +  timeInAir);
+
+			if (deathWhenGround){
+				deathWhenGround = false;
+				Die();
+			}
+
 		} else {
 			timeInAir += Time.deltaTime;
 		}
+
+
+		if (timeInAir >= FallingDeathTimer){
+			deathWhenGround = true;
+		}
+
 		float h = Input.GetAxis ("Horizontal");
 		if (h < -0.1f) {//Movimiento izda
 			transform.localScale = new Vector3(-1f,1f,1f);
@@ -93,12 +103,8 @@ public class PlayerController : MonoBehaviour {
 		//aura = GetComponents<Light>()[0];
 		//Debug.Log ( GetComponentsInParent<Light>().Length);
 
-		IncreaseLight ();
+		//IncreaseLight ();
 
-		if (timeInAir >= FallingDeathTimer) 
-		{
-			Die();
-		}
 
 	}
 
